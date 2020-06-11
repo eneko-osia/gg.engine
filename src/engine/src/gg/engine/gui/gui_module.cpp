914aba04373e11cb541c8d090d441ba0937b8158
eneko-osia <eneko.osia@gmail.com>
2020-06-11 17:52:45 +0200

    [engine] updated

--- src/engine/src/gg/engine/gui/gui_module.cpp
+++ src/engine/src/gg/engine/gui/gui_module.cpp
@@ -17,3 +17,47 @@ bool8 gui_module::on_init(void) noexcept
 //==============================================================================
 }
 //==============================================================================
+
+/*
+class pid_controller
+{
+public:
+
+    // constructors
+
+    pid_controller(float32 p, float32 i, float32 d) noexcept
+        : k_proportional(p)
+        , k_integral(i)
+        , k_derivative(d)
+        , error(0.0f)
+        , integral(0.0f)
+    {
+    }
+
+    // methods
+
+    float32 calculate(float32 elapsed, float32 goal, float32 actual) noexcept
+    {
+        float32 current_error = goal - actual;
+        float32 current_integral = integral + (current_error * elapsed);
+        float32 current =
+            (k_proportional * current_error) +
+            (k_integral * current_integral) +
+            (k_derivative * ((current_error - error) / elapsed));
+        error = current_error;
+        integral = current_integral;
+        return current;
+    }
+
+private:
+
+    // attributes
+
+    float32 const k_proportional;
+    float32 const k_integral;
+    float32 const k_derivative;
+
+    float32 error;
+    float32 integral;
+};
+*/
